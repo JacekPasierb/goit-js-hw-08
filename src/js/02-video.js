75758800529783d8c8cbf2ca20d11db31969a86c
Jacek Pasierb <jack_byk@o2.pl>
2023-03-03 17:09:19 +0100

    Created first code for task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+import Vimeo from '@vimeo/player';
 
+
+
+
+
+const player = new Vimeo('vimeo-player', {
+  id: 236203659,
+  width: 640,
+  height: 360,
+});
+
+ player.on('play', function () {
+   console.log('played the video!');
+ });
+
+ player.getVideoTitle().then(function (title) {
+   console.log('title:', title);
+ });
+
+ 
