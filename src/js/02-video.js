b9009999fc26dab9bed5cbc9c4339e72e3907f91
Jacek Pasierb <jack_byk@o2.pl>
2023-03-04 17:45:00 +0100

    Created code for timeupdate

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,21 +1,22 @@
 import Vimeo from '@vimeo/player';
 
 
-
-
-
-const player = new Vimeo('vimeo-player', {
-  id: 236203659,
-  width: 640,
-  height: 360,
-});
+const iframe = document.querySelector("iframe");
+const player = new Vimeo(iframe);
 
  player.on('play', function () {
    console.log('played the video!');
  });
 
+
  player.getVideoTitle().then(function (title) {
    console.log('title:', title);
  });
 
+ // nasłuchujemy wydarzenie
+player.on('timeupdate', (e) => {
  
+  console.log(e);
+  localStorage.setItem('videoplayer-current-time', JSON.stringify(e));
+   // data is an object containing properties specific to that event
+ });
